aea5c87708c3ae7a9b8679bd0fc24d99113645e1
manwithsimplebrain <manwithsimplebrain@gmail.com>
2025-02-06 14:55:29 +0700

    Init project

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Dat on 06/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
